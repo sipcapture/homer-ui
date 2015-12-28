fa9138cb368533fca99946c86eede5e001042e99
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2015-12-28 16:45:22 +0100

    moved datasource to external js file

--- js/widgets/sipcapture/sipcapture.js
+++ js/widgets/sipcapture/sipcapture.js
@@ -103,7 +103,7 @@ angular.module("homer.widgets.sipcapture", [ "adf.provider", "highcharts-ng", "a
     }
     var rangeDate = {};
     var data = {};
-    var fields = $scope.config.panel.filters;
+    var fields = $scope.config.panel ? $scope.config.panel.filters : [];
 
     $scope.reloadIt = function() {
         console.log("reloading");
@@ -281,6 +281,7 @@ angular.module("homer.widgets.sipcapture", [ "adf.provider", "highcharts-ng", "a
                         $scope.chartConfig.plotOptions.pie.dataLabels.enabled = false;
                     }
                 }
+
             } else {
                 $scope.chartConfig = {
                     chart: {
@@ -475,7 +476,7 @@ angular.module("homer.widgets.sipcapture", [ "adf.provider", "highcharts-ng", "a
         if ($scope.config.chart.type.value == 'pie') {
             $scope.config.panel.total = true;
         } else {
-            $scope.config.panel.total = false;
+            if ($scope.config.panel) $scope.config.panel.total = false;
         }
     };
 
@@ -483,7 +484,7 @@ angular.module("homer.widgets.sipcapture", [ "adf.provider", "highcharts-ng", "a
     // Select Engine
     //------------------------------------------------------------------------------------------
     $scope.selectEngine = function() {
-        $scope.config.chart.update.clear();
+        if ($scope.config.chart.update) $scope.config.chart.update.clear();
     };
 
     //==========================================================================================
@@ -1454,163 +1455,8 @@ sipcaptureWdgt.colorRandom = function (y) {
 ////////////////////////////////////////////////////////////////////////////////////////////
 sipcaptureWdgt.data = {}
 
-sipcaptureWdgt.data.datasources = {
-    "version": 1,
-    "datasources": [
-        {
-            "name": "Method",
-            "type": "JSON",
-            "settings": {
-                "path": "statistic\/method",
-                "query": "{\n   \"timestamp\": {\n          \"from\": \"@from_ts\",\n          \"to\":  \"@to_ts\"\n   },\n  \"param\": {\n        \"filter\": [ \n             \"@filters\"\n       ],\n       \"limit\": \"@limit\",\n       \"total\": \"@total\"\n   }\n}",
-                "method": "GET",
-                "limit": 200,
-                "total": false,
-                "eval": {
-                    incoming: {
-                        name: "test incoming",
-                        value: "var object = @incoming; return object"
-                    }
-                },
-                "timefields" : [
-                    { "field": "from_ts", "desc": "From Timestamp" },
-                    { "field": "to_ts", "desc": "To Timestamp" }
-                ],
-                "fieldvalues": [
-                    { "field": "total", "desc": "All Packets" }
-                ],
-                "filters": [
-			{ "type": "method", "desc": "SIP Method", options: [ {"value": "!ALL"},{"value":"!TOTAL"},{"value": "TOTAL"},{"value": "INVITE"},{"value": "UPDATE"}, {"value": "REGISTER"}, {"value": "CANCEL"}, {"value": "BYE"}, {"value": "OPTIONS"}, {"value": "300"}, {"value": "401"}, {"value": "407"}, {"value": "200"} ] },
-			{ "type": "cseq", "desc": "SIP Cseq", options: [ {"value": "INVITE"}, {"value": "UPDATE"},{"value": "REGISTER"},{"value": "CANCEL"}, {"value": "BYE"}, {"value": "OPTIONS"} ] },
-			{ "type": "auth", "desc": "SIP Auth", options: [ {"value": "true"} ] },
-			{ "type": "totag", "desc": "SIP To Tag", options: [ {"value": "true"} ] }
-                ]
-            }
-        },
-        {
-            "name": "Data",
-            "type": "JSON",
-            "settings": {
-                "path": "statistic\/data",
-                        "query": "{\n   \"timestamp\": {\n          \"from\": \"@from_ts\",\n          \"to\":  \"@to_ts\"\n   },\n  \"param\": {\n        \"filter\": [ \n             \"@filters\"\n       ],\n       \"limit\": \"@limit\",\n       \"total\": \"@total\"\n   }\n}",
-                "method": "GET",
-                "limit": 200,
-                "total": false,
-                "eval": {
-                    incoming: {
-                        name: "test incoming",
-                        value: "var object = @incoming; return object"
-                    }
-                },
-                "timefields" : [
-                    { "field": "from_ts", "desc": "From Timestamp" },
-                    { "field": "to_ts", "desc": "To Timestamp" }
-                ],
-                "fieldvalues": [
-                    { "field": "total", "desc": "All Packets" }
-                ],
-                "filters": [
-		    { "type": "type", "desc": "Data Statistics", options: [
-	                    { "value": "asr", "desc": "Answer Seizure Ratio" },
-	                    { "value": "ner", "desc": "Network Effectiveness Ratio" },
-	                    { "value": "packet_size", "desc": "Packet Size" },
-	                    { "value": "packet_count", "desc": "Packet Count" },
-	                    { "value": "sdf", "desc": "counter of call’s releases except busy and normal call clearing (17 && 16)" },
-	                    { "value": "isa", "desc": "counter of replies on INVITE: 408|50[03]" },
-	                    { "value": "sd", "desc": "counter of replies on INVITE: 50[034]" },
-	                    { "value": "ssr", "desc": "call success setup rate" }
-			] 
-		    }
-                ]
-            }
-        },
-        {
-            "name": "IP",
-            "type": "JSON",
-            "settings": {
-                "path": "statistic\/ip",
-                "query": "{\n   \"timestamp\": {\n          \"from\": \"@from_ts\",\n          \"to\":  \"@to_ts\"\n   },\n  \"param\": {\n        \"filter\": [ \n             \"@filters\"\n       ],\n       \"limit\": \"@limit\",\n       \"total\": \"@total\"\n   }\n}",
-                "method": "GET",
-                "limit": 200,
-                "total": false,
-                "eval": {
-                    incoming: {
-                        name: "test incoming",
-                        value: "var object = @incoming; return object"
-                    }
-                },
-                "timefields" : [
-                    { "field": "from_ts", "desc": "From Timestamp" },
-                    { "field": "to_ts", "desc": "To Timestamp" }
-                ],
-                "fieldvalues": [
-                    { "field": "total", "desc": "All Packets" }
-                ],
-                "filters": [
-  		    { "type": "method", "desc": "SIP Method", options: [ {"value": "!ALL"},{"value":"!TOTAL"},{"value": "TOTAL"},{"value": "INVITE"},{"value": "UPDATE"}, {"value": "REGISTER"}, {"value": "CANCEL"}, {"value": "BYE"}, {"value": "OPTIONS"}, {"value": "300"}, {"value": "401"}, {"value": "407"}, {"value": "200"} ] },
-                    { "type": "source_ip", "desc": "SIP Source IP", options: [] }
-                ]
-            }
-        },
-        {
-            "name": "User-Agent",
-            "type": "JSON",
-            "settings": {
-                "path": "statistic\/useragent",
-                "query": "{\n   \"timestamp\": {\n          \"from\": \"@from_ts\",\n          \"to\":  \"@to_ts\"\n   },\n  \"param\": {\n        \"filter\": [ \n             \"@filters\"\n       ],\n       \"limit\": \"@limit\",\n       \"total\": \"@total\"\n   }\n}",
-                "method": "GET",
-                "limit": 200,
-                "total": false,
-                "eval": {
-                    incoming: {
-                        name: "test incoming",
-                        value: "var object = @incoming; return object"
-                    }
-                },
-                "timefields" : [
-                    { "field": "from_ts", "desc": "From Timestamp" },
-                    { "field": "to_ts", "desc": "To Timestamp" }
-                ],
-                "fieldvalues": [
-                    { "field": "total", "desc": "All Packets" }
-                ],
-                "filters": [
-  		    { "type": "useragent", "desc": "SIP User-Agent", options: [ {"value": "!ALL"},{"value":"!TOTAL"},{"value": "TOTAL"},{"value": "INVITE"},{"value": "UPDATE"}, {"value": "REGISTER"}, {"value": "CANCEL"}, {"value": "BYE"}, {"value": "OPTIONS"}, {"value": "300"}, {"value": "401"}, {"value": "407"}, {"value": "200"} ] },
-  		    { "type": "method", "desc": "SIP Method", options: [ {"value": "!ALL"},{"value":"!TOTAL"},{"value": "TOTAL"},{"value": "INVITE"},{"value": "UPDATE"}, {"value": "REGISTER"}, {"value": "CANCEL"}, {"value": "BYE"}, {"value": "OPTIONS"}, {"value": "300"}, {"value": "401"}, {"value": "407"}, {"value": "200"} ] }
-                ]
-            }
-        },
-        {
-            "name": "Alarms",
-            "type": "JSON",
-            "settings": {
-                "path": "statistic\/alarm",
-                "query": "{\n   \"timestamp\": {\n          \"from\": \"@from_ts\",\n          \"to\":  \"@to_ts\"\n   },\n  \"param\": {\n        \"filter\": [ \n             \"@filters\"\n       ],\n       \"limit\": \"@limit\",\n       \"total\": \"@total\"\n   }\n}",
-                "method": "GET",
-                "limit": 200,
-                "total": false,
-                "eval": {
-                    incoming: {
-                        name: "test incoming",
-                        value: "var object = @incoming; return object"
-                    }
-                },
-                "timefields" : [
-                    { "field": "from_ts", "desc": "From Timestamp" },
-                    { "field": "to_ts", "desc": "To Timestamp" }
-                ],
-                "fieldvalues": [
-                    { "field": "total", "desc": "All Alarms" }
-                ],
-                "filters": [
-  		    { "type": "source_ip", "desc": "Source IP", options: [ {"value": "0.0.0.0"} ] },
-  		    { "type": "type", "desc": "Alarm Type", options: [ {"value": "scanner"} ] }
-                ]
-            }
-        }
-    
-    ]
-};
+// External datasource: js/widgets/datasource.js
+sipcaptureWdgt.data.datasources = window.datasource_h5;
 
 //==========================================================================================
 // Adding toggleText to jquery
