576797d9b5cbc6bca32e2638e0b2e3c7983342c1
Rudolph Bott <bott@sipgate.de>
2016-02-04 16:00:40 +0100

    add orand parameter to GET search result request

--- js/modules/pages/controllers/resultCtrl.js
+++ js/modules/pages/controllers/resultCtrl.js
@@ -125,6 +125,7 @@
                     if(sObj.hasOwnProperty("search_ruri_user")) searchValue["ruri_user"] = sObj["search_ruri_user"];
                     if(sObj.hasOwnProperty("search_from_user")) searchValue["from_user"] = sObj["search_from_user"];
                     if(sObj.hasOwnProperty("search_to_user")) searchValue["to_user"] = sObj["search_to_user"];
+                    if(sObj.hasOwnProperty("search_orand")) searchValue["orand"] = sObj["search_orand"];
 
                     data.param.limit = limit;
                     data.param.search = searchValue;
