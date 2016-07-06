b7ec802facf9ce1f7fb78ba129257a130e251676
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-07-06 16:10:21 +0200

    Fix for #175

--- share/controller.js
+++ share/controller.js
@@ -612,7 +612,31 @@
                         $(element)
                         .draggable({
                               cancel: ".homer-modal-body, .close",
-                              handle: ".homer-modal-header"
+                              handle: ".homer-modal-header",
+                              start: function( event, ui ) {
+                                ui.helper[0].style.opacity=0.7;
+                                var i = 0;
+                                var elements=document.getElementsByClassName('opened');
+                                for(i=0;i<elements.length;i++){if(!elements[i].style.zIndex){elements[i].style.zIndex=10001;}}
+                                if (window.topZ){window.topZ++;ui.helper[0].style.zIndex = window.topZ; }
+                                else{ui.helper[0].style.zIndex++;window.topZ = ui.helper[0].style.zIndex;}
+                              },
+                              stop: function( event, ui ) {
+                                if (ui.helper[0]) {
+                                        ui.helper[0].style.opacity=1;
+                                        if (ui.offset.top < 0) ui.helper[0].style.top = '0px';
+                                        if (ui.offset.left < 0) ui.helper[0].style.left = '0px';
+                                        if ((ui.offset.left + 100 ) > window.innerWidth) {
+                                                ui.helper[0].style.left = (window.innerWidth - 100)+'px';
+                                                window.scrollTo(0, 0);
+                                        }
+                                        if ((ui.offset.top + 50 ) > window.innerHeight) {
+                                                ui.helper[0].style.top = (window.innerHeight - 50)+'px';
+                                                window.scrollTo(0, 0);
+                                        }
+
+                                }
+                              }
                         })
                         .resizable({
                             resize: function (evt, ui) {
