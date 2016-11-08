983eda11a045c83a62e7a23e15e380109775e83c
twmartin <ty.w.martin@gmail.com>
2016-11-08 15:26:23 -0600

    Standardize whitespace

--- js/widgets/clock/timezones.js
+++ js/widgets/clock/timezones.js
@@ -1,10 +1,10 @@
-    var timeZoneData = {
-	  "UTC/UTC": 0,
-	  "Europe/London": 0,
-	  "Europe/Amsterdam": 1,
-	  "America/Los_Angeles":-8,
-    "America/Minneapolis":-6,
-	  "America/New_York":-5,
-	  "Europe/Moscow":+3,
-	  "Asia/Novosibirsk":+6
-    };
+var timeZoneData = {
+  "UTC/UTC": 0,
+  "Europe/London": 0,
+  "Europe/Amsterdam": 1,
+  "America/Los_Angeles":-8,
+  "America/Minneapolis":-6,
+  "America/New_York":-5,
+  "Europe/Moscow":+3,
+  "Asia/Novosibirsk":+6
+};
