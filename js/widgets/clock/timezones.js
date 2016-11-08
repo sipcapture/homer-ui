a4a11e197f64ba7cd0e63e4e50cdaf506040b389
twmartin <ty.w.martin@gmail.com>
2016-11-08 15:22:59 -0600

    Add Central timezone

--- js/widgets/clock/timezones.js
+++ js/widgets/clock/timezones.js
@@ -3,6 +3,7 @@
 	  "Europe/London": 0,
 	  "Europe/Amsterdam": 1,
 	  "America/Los_Angeles":-8,
+    "America/Minneapolis":-6,
 	  "America/New_York":-5,
 	  "Europe/Moscow":+3,
 	  "Asia/Novosibirsk":+6
