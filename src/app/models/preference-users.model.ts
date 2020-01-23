9b023194d8b43e9e65269d96a7fc35696b119a16
Eduard Klimenko <edik.klimenko@gmail.com>
2020-01-23 19:05:50 +0200

    protected: User name / Group / Partition ID

--- src/app/models/preference-users.model.ts
+++ src/app/models/preference-users.model.ts
@@ -2,7 +2,7 @@ export interface PreferenceUsers {
     username: string;
     partid: number;
     email: string;
-    password: string;
+    password?: string;
     firstname: string;
     lastname: string;
     department: string;
