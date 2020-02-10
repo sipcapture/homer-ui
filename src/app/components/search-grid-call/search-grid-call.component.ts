262cd9ebbf439de4e5cb31714788a56428caeef5
Eduard Klimenko <edik.klimenko@gmail.com>
2020-02-10 12:13:50 +0200

    clear console log

--- src/app/components/search-grid-call/search-grid-call.component.ts
+++ src/app/components/search-grid-call/search-grid-call.component.ts
@@ -494,7 +494,6 @@ export class SearchGridCallComponent implements OnInit, OnDestroy, AfterViewInit
 
         if ( this.isLokiQuery ) {
             this._srs.getData(this.queryBuilderForLoki()).toPromise().then(result => {
-                console.log('LOKI result.data', result.data);
                 this.rowData = result.data.sort(( a, b ) => {
                     a = new Date(a.micro_ts).getTime();
                     b = new Date(b.micro_ts).getTime();
