c0653c35989412deb7f8ca66fa08648ff4198b2c
Eduard Klimenko <edik.klimenko@gmail.com>
2020-02-10 15:02:05 +0200

    fix: https://github.com/sipcapture/homer-app/issues/281 Loki search results HTML sanitization

--- src/app/components/search-grid-call/renderer/loki-highlight-renderer.component.ts
+++ src/app/components/search-grid-call/renderer/loki-highlight-renderer.component.ts
@@ -1,13 +1,11 @@
 import {Component} from '@angular/core';
 import {ICellRendererAngularComp} from 'ag-grid-angular';
 
-
-
 @Component({
     template: `<div class="loki-highlight" [innerHTML]="data"></div>`
 })
 
-export class LokiHighlightRenderer implements ICellRendererAngularComp{
+export class LokiHighlightRenderer implements ICellRendererAngularComp {
     public params: any;
     data: string;
 
@@ -16,15 +14,15 @@ export class LokiHighlightRenderer implements ICellRendererAngularComp{
         const rxText = this.params.context.componentParent.searchQueryLoki.rxText;
         if (!!rxText) {
             const regex = new RegExp('(' + rxText + ')', 'g');
-            this.data = this.params.value
-                .replace(/\</g, '&lt;')
-                .replace(/\>/g, '&gt;')
-                .replace(regex, (g, a) => {
-                    return `<span>${a}</span>`;
-                });
+            this.data = this.htmlSpecialChars(this.params.value)
+                .replace(regex, (g, a) => `<span>${a}</span>`);
         } else {
-            this.data = this.params.value || '';
+            this.data = this.htmlSpecialChars(this.params.value || '');
         }
+    }
+    private htmlSpecialChars(s: string) {
+        return s.replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+
     }
     refresh(): boolean {
         return false;
