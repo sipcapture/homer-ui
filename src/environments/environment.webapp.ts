5b150698b72d81a8be0d85c67b2fd26c398807d0
Joel Guerra <jacovinus@de9.sipcapture.io>
2020-03-13 10:58:06 +0100

    Proto Search UI improve

--- src/environments/environment.webapp.ts
+++ src/environments/environment.webapp.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   environment: 'env::webapp',
-  //apiUrl: window.location.protocol + '//127.0.0.1:9080/api/v3'
-  apiUrl: 'http://de9.sipcapture.io/api/v3'
+  apiUrl: window.location.protocol + '//127.0.0.1:9080/api/v3'
 };
