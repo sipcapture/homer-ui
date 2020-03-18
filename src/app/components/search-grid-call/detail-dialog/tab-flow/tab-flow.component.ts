fa445c3ef18fdde551bf33b20cc976e1234b9bdd
Eduard Klimenko <edik.klimenko@gmail.com>
2020-03-18 18:50:16 +0200

    fix: https://github.com/sipcapture/homer-app/issues/306 PNG export does not show IP addresses

--- src/app/components/search-grid-call/detail-dialog/tab-flow/tab-flow.component.ts
+++ src/app/components/search-grid-call/detail-dialog/tab-flow/tab-flow.component.ts
@@ -41,11 +41,11 @@ export class TabFlowComponent implements OnInit, AfterViewInit, OnDestroy {
         return this._dataItem;
     }
     @Input() set exportAsPNG(val) {
-        if(val) {
+        if (val) {
             this.isExport = true;
             setTimeout(() => {
                 this.onSavePng();
-            });
+            }, 500);
         }
     }
     @Output() messageWindow: EventEmitter<any> = new EventEmitter();
@@ -172,33 +172,31 @@ export class TabFlowComponent implements OnInit, AfterViewInit, OnDestroy {
     * @param {bool} reverse false - reverse sorting.
     * @returns {Array} array of items in [[key,value],[key,value],...] format.
     */
-    sortProperties(obj, sortedBy, isNumericSort, reverse) {
-            sortedBy = sortedBy || 1; // by default first key
-            isNumericSort = isNumericSort || false; // by default text sort
-            reverse = reverse || false; // by default no reverse
+    sortProperties(obj: any, sortedBy: any = 1, isNumericSort = false, reverse = false) {
+        const reversed = (reverse) ? -1 : 1;
+        const sortable = [];
 
-            var reversed = (reverse) ? -1 : 1;
-
-            var sortable = [];
-            for (var key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                    sortable.push([key, obj[key]]);
-                }
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                sortable.push([key, obj[key]]);
             }
-            if (isNumericSort)
-                sortable.sort(function (a, b) {
-                    return reversed * (a[1][sortedBy] - b[1][sortedBy]);
-                });
-            else
-                sortable.sort(function (a, b) {
-                    var x = a[1][sortedBy].toLowerCase(),
-                        y = b[1][sortedBy].toLowerCase();
-                    return x < y ? reversed * -1 : x > y ? reversed : 0;
-                });
-            return sortable.reduce((obj, item) => {
-              obj[item[0]] = item[1]
-              return obj
-            }, {})
+        }
+        if (isNumericSort) {
+            sortable.sort(function (a, b) {
+                return reversed * (a[1][sortedBy] - b[1][sortedBy]);
+            });
+        } else {
+            sortable.sort(function (a, b) {
+                const x = a[1][sortedBy].toLowerCase();
+                const y = b[1][sortedBy].toLowerCase();
+                return x < y ? reversed * -1 : x > y ? reversed : 0;
+            });
+        }
+
+        return sortable.reduce((target, item) => {
+            target[item[0]] = item[1];
+            return target;
+        }, {});
     }
 
     onSavePng() {
