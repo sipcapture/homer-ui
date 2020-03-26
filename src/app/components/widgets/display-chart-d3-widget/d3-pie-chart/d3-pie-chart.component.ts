7fb901f960a6aded9ed38c59c5fa0834ae78f847
jacovinus <jacovinus@gmail.com>
2020-03-26 09:47:37 +0100

    code clean

--- src/app/components/widgets/display-chart-d3-widget/d3-pie-chart/d3-pie-chart.component.ts
+++ src/app/components/widgets/display-chart-d3-widget/d3-pie-chart/d3-pie-chart.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ElementRef, ViewEncapsulation, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, ElementRef, ViewEncapsulation, Input, SimpleChanges, OnChanges } from '@angular/core';
+
 export class DonutChartDatum {
   code: string;
   displayValue: string;
   count: number;
 }
-/** @todo modify the color range array with the settings of the widget */
+// import the d3 library
 import * as d3 from 'd3';
 
 
@@ -14,13 +15,12 @@ import * as d3 from 'd3';
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./d3-pie-chart.component.scss']
 })
-export class D3PieChartComponent implements OnInit, OnChanges {
+export class D3PieChartComponent implements OnChanges {
 
-@Input() data:number[];
-@Input() colorScales:string[];
-@Input() keysSet:string[];
+  @Input() data:number[];
+  @Input() colorScales:string[];
+  @Input() keysSet:string[];
 
-  //@Input() chartColorScale: string[];
   hostElement; // Native element hosting the SVG container
   svg; // Top level SVG element
   g; // SVG Group element
@@ -35,11 +35,8 @@ export class D3PieChartComponent implements OnInit, OnChanges {
   colorScale; // D3 color provider
   pieData: any; // Arc segment parameters for current data set
   pieDataPrevious: any; // Arc segment parameters for previous data set - used for transitions
-  colors = d3.scaleOrdinal(d3.schemeCategory10);
 
-  // Pie function - transforms raw data to arc segment parameters
-  dataArray = [];
-  pie = d3.pie()
+  pie = d3.pie() 
       .startAngle(-1 * Math.PI)
       .endAngle(1 * Math.PI)
       .sort(null)
@@ -49,38 +46,27 @@ export class D3PieChartComponent implements OnInit, OnChanges {
       this.hostElement = this.elRef.nativeElement;
   }
 
-  ngOnInit() {
-   }
-
   ngOnChanges(changes: SimpleChanges) {
       if (changes.data) {
         this.updateChart(changes.data.currentValue);
       }
   }
 
+/* main chart generator */
+
   private createChart(data:number[]) {
-
     this.processPieData(data);
-
-      this.removeExistingChartFromParent();
-
-      this.setChartDimensions();
-
-      this.setColorScale();
-    
-      this.addGraphicsElement();
-
-      this.setupArcGenerator();
-
-      this.addSlicesToTheDonut();
-
-      this.addLabelsToTheDonut();
-
-      this.addDonutTotalLabel();
-
-      
-
+    this.removeExistingChartFromParent();
+    this.setChartDimensions();
+    this.setColorScale();
+    this.addGraphicsElement();
+    this.setupArcGenerator();
+    this.addSlicesToTheDonut();
+    this.addLabelsToTheDonut();
+    this.addDonutTotalLabel();
   }
+
+  /*  chart param generators */
 
   private setChartDimensions() {
       let viewBoxHeight = 125;
@@ -89,30 +75,27 @@ export class D3PieChartComponent implements OnInit, OnChanges {
           .attr('width', '100%')
           .attr('height', '100%')
           .attr('viewBox', '40 0 ' + viewBoxWidth + ' ' + viewBoxHeight);
-  }
+    }
 
-  private addGraphicsElement() {
-      this.g = this.svg.append("g")
-          .attr("transform", "translate(100,50)");
-  }
+    private addGraphicsElement() {
+        this.g = this.svg.append("g")
+            .attr("transform", "translate(100,50)");
+    }
 
+    private setColorScale() {
+        this.colorScale = d3.scaleOrdinal()
+        .domain([...this.keysSet])
+        .range([...this.colorScales]);
+    }
 
-  private setColorScale() {
-
-    this.colorScale = d3.scaleOrdinal()
-    .domain([...this.keysSet])
-    .range([...this.colorScales]);
-    
-  }
-
-  private processPieData(data, initial = true) {
-      this.rawData = data;
-      this.total = this.rawData.reduce((sum, next) => sum + next, 0);
-      this.pieData = this.pie(data);
-      if (initial) {
-          this.pieDataPrevious = this.pieData;
-      }
-  }
+    private processPieData(data, initial = true) {
+        this.rawData = data;
+        this.total = this.rawData.reduce((sum, next) => sum + next, 0);
+        this.pieData = this.pie(data);
+        if (initial) {
+            this.pieDataPrevious = this.pieData;
+        }
+    }
 
   private setupArcGenerator() {
       this.innerRadius = 30;
@@ -120,7 +103,7 @@ export class D3PieChartComponent implements OnInit, OnChanges {
       this.arc = d3.arc()
           .innerRadius(this.innerRadius)
           .outerRadius(this.radius)
-  }
+    }
 
   private addSlicesToTheDonut() {
       this.slices = this.g.selectAll('allSlices')
@@ -136,15 +119,16 @@ export class D3PieChartComponent implements OnInit, OnChanges {
               return this.colorScale(`${index}`);
           })
           .attr('stroke-width','.35')
+          // hover event
           .on('mouseover', async function (d, i) {
               let keys = await this.keysSet;
             d3.select(this)
                  .attr('fill-opacity', '.75')
           })
-       .on('mouseout', function (d, i) {
-            d3.select(this)
-                 .attr('fill-opacity', '.45');
-       })
+            .on('mouseout', function (d, i) {
+                    d3.select(this)
+                        .attr('fill-opacity', '.45');
+            })
   }
 
   private addDonutTotalLabel() {
@@ -161,9 +145,7 @@ export class D3PieChartComponent implements OnInit, OnChanges {
         
   }
 
-  // Creates an "interpolator" for animated transition for arc slices
-  //   given previous and new arc shapes,
-  //   generates a series of arc shapes (be)tween start and end state
+
   arcTween = (datum, index) => {
       const interpolation = d3.interpolate(this.pieDataPrevious[index], datum);
       this.pieDataPrevious[index] = interpolation(0);
@@ -172,9 +154,6 @@ export class D3PieChartComponent implements OnInit, OnChanges {
       }
   }
 
-  // Creates an "interpolator" for animated transition for arc labels
-  //   given previous and new label positions,
-  //   generates a series of arc states (be)tween start and end state
   labelTween = (datum, index) => {
       const interpolation = d3.interpolate(this.pieDataPrevious[index], datum);
       this.pieDataPrevious[index] = interpolation(0);
@@ -183,18 +162,41 @@ export class D3PieChartComponent implements OnInit, OnChanges {
       }
   }
 
+  private addLabelsToTheDonut() {
+    this.labels = this.g
+        .selectAll('allLabels')
+        .data(this.pieData)
+        .enter()
+        .append('text')
+        .text(this.labelValueGetter)
+        .attr('transform', (datum, index) => {
+            return 'translate(' + this.arc.centroid(datum) + ')';
+        })
+        .style('font-size', '4px')
+        .style('text-anchor', 'middle')
+        .style('fill','#52545c')
+
+    }
+
+    private labelValueGetter = (datum, index) => {
+        return this.labelValueFn(this.rawData[index]);
+    }
+
+    private labelValueFn(val) {
+        const pct = Math.floor(val * 100 / this.total);
+        return (pct < 4) ? '' : '' + val;
+    }
+
+ /* update chart methods  after event */
+
   public updateChart(data:number[]) {
       if (!this.svg) {
           this.createChart(data);
           return;
       }
-
       this.processPieData(data, false);
-
       this.updateSlices();
-
       this.updateLabels();
-
       this.createChart(data);
 
   }
@@ -218,36 +220,7 @@ export class D3PieChartComponent implements OnInit, OnChanges {
   }
 
   private removeExistingChartFromParent() {
-      // !!!!Caution!!!
-      // Make sure not to do;
-      //     d3.select('svg').remove();
-      // That will clear all other SVG elements in the DOM
       d3.select(this.hostElement).select('svg').remove();
   }
-
-  private addLabelsToTheDonut() {
-      this.labels = this.g
-          .selectAll('allLabels')
-          .data(this.pieData)
-          .enter()
-          .append('text')
-          .text(this.labelValueGetter)
-          .attr('transform', (datum, index) => {
-              return 'translate(' + this.arc.centroid(datum) + ')';
-          })
-          .style('font-size', '4px')
-          .style('text-anchor', 'middle')
-          .style('fill','#52545c')
-
-  }
-
-  private labelValueGetter = (datum, index) => {
-      return this.labelValueFn(this.rawData[index]);
-  }
-
-  private labelValueFn(val) {
-      const pct = Math.floor(val * 100 / this.total);
-      return (pct < 4) ? '' : '' + val;
-  }
-
+  
 }
--- src/app/components/widgets/display-chart-d3-widget/display-chart-d3-widget.component.ts
+++ src/app/components/widgets/display-chart-d3-widget/display-chart-d3-widget.component.ts
@@ -18,7 +18,6 @@ export class Tag {
   count: number;
 }
 
-
 @Component({
   selector: 'app-display-chart-d3',
   templateUrl: './display-chart-d3-widget.component.html',
@@ -35,144 +34,127 @@ export class Tag {
 
 export class DisplayChartD3WidgetComponent implements IWidget {
 
-@ViewChild('d3Chart', { static: true }) chart: D3PieChartComponent;
-chartKeys: Tag[];
+  @ViewChild('d3Chart', { static: true }) chart: D3PieChartComponent;
+  @Input() id: string;
+  @Input() config: any;
+  @Output() changeSettings = new EventEmitter < any > ();
 
-chartData: number[] = [];
+  title: string;
+  chartKeys: Tag[];
+  chartData: number[] = [];
 
-configQuery = {
-  param: {
-    transaction: {},
-    limit: 200,
-    search: {},
-    location: {},
-    timezone: {
-      value: -180,
-      name: 'Local'
+  configQuery = {
+    param: {
+      transaction: {},
+      limit: 200,
+      search: {},
+      location: {},
+      timezone: {
+        value: -180,
+        name: 'Local'
+      }
+    },
+    timestamp: {
+      from: 0,
+      to: 0
     }
-  },
-  timestamp: {
-    from: 0,
-    to: 0
-  }
-};
+  };
 
-Tags = {
-  tagObj:[
-    {
-    count:0,
-    color:'',
-    name:'',
-  }
-]
-}
-
-@Input() id: string;
-@Input() config: any;
-@Output() changeSettings = new EventEmitter < any > ();
-
-title: string;
-
-_lastTimeStamp = 0;
-set lastTimestamp(val: number) {
-  this._lastTimeStamp = val;
-}
-get lastTimestamp() {
-  return this._lastTimeStamp;
-}
-
-columnKeysGroupColumn;
-
-localData: any;
-
-protocol_profile
-
-dataForChart
-
-groupColumnAxis1: string;
-
-noChartData: false;
-
-isShowPanelSettings = true;
-
-columnKeys = [];
-
-keySelected = 'method';   //--- >  Key selected on the select of the chart
-
-keysSet = [];
-
-colorScale = [];
-
-selectedKeyArr = [];
-
-subsDashboardEvent: Subscription;
-
-subsCastRangeUpdateTimeOut: Subscription;
-
-constructor(
-    public dialog: MatDialog,
-    private _dashboardService : DashboardService,
-    private _prefMapProtoService : PreferenceMappingProtocolService,
-    private _srcCallService : SearchCallService,
-    private _dateTimeRageService : DateTimeRangeService,
-    public chartControlsService: ChartControlsService) { 
-    this.chartControlsService.fullScreen = false;
+  Tags = {
+    tagObj:[
+      {
+      count:0,
+      color:'',
+      name:'',
+    }
+  ]
   }
 
-ngOnInit() {
-    WidgetArrayInstance[this.id] = this as IWidget; 
-    this.config = {
-      name: 'display-chart-d3',
-      configQuery: this.configQuery
+  _lastTimeStamp = 0;
+  set lastTimestamp(val: number) {
+    this._lastTimeStamp = val;
+  }
+  get lastTimestamp() {
+    return this._lastTimeStamp;
+  }
+
+  columnKeysGroupColumn; 
+  localData: any; 
+  protocol_profile 
+  dataForChart
+  groupColumnAxis1: string; 
+  noChartData: false;
+  isShowPanelSettings = true;
+  columnKeys = [];
+  keySelected = 'method';   // default key selected
+  keysSet = [];
+  colorScale = [];
+  selectedKeyArr = [];
+  subsDashboardEvent: Subscription;
+  subsCastRangeUpdateTimeOut: Subscription;
+
+  constructor(
+      public dialog: MatDialog,
+      private _dashboardService : DashboardService,
+      private _prefMapProtoService : PreferenceMappingProtocolService,
+      private _srcCallService : SearchCallService,
+      private _dateTimeRageService : DateTimeRangeService,
+      public chartControlsService: ChartControlsService) { 
+      this.chartControlsService.fullScreen = false;
     }
 
-    if (!this.configQuery) {
-      this.configQuery = {
-        param: {
-          transaction: {},
-          limit: 200,
-          search: {},
-          location: {},
-          timezone: {
-          value: -180,
-          name: 'Local'
+  ngOnInit() {
+      WidgetArrayInstance[this.id] = this as IWidget; 
+      this.config = {
+        name: 'display-chart-d3',
+        configQuery: this.configQuery
+      }
+
+      if (!this.configQuery) {
+        this.configQuery = {
+          param: {
+            transaction: {},
+            limit: 200,
+            search: {},
+            location: {},
+            timezone: {
+            value: -180,
+            name: 'Local'
+            }
+          },
+          timestamp: {
+            from: 0,
+            to: 0
           }
-        },
-        timestamp: {
-          from: 0,
-          to: 0
-        }
-      };
-    }
+        };
+      }
 
+    const _f = Functions.cloneObject;
 
-  const _f = Functions.cloneObject;
     if (this.config) {
-      this.title = this.config.title || 'DISPLAY-CHART-D3';
-      this.configQuery = _f(this.config.configQuery);
-      this.groupColumnAxis1 = this.config.groupColumnAxis1;
-      this.localData = _f(this.config.localData);
-      this.isShowPanelSettings = this.config.isShowPanelSettings !== null ? this.config.isShowPanelSettings : true;
+        this.title = this.config.title || 'DISPLAY-CHART-D3';
+        this.configQuery = _f(this.config.configQuery);
+        this.groupColumnAxis1 = this.config.groupColumnAxis1;
+        this.localData = _f(this.config.localData);
+        this.isShowPanelSettings = this.config.isShowPanelSettings !== null ? this.config.isShowPanelSettings : true;
     }
     this.subsDashboardEvent = this._dashboardService.dashboardEvent.subscribe(this.onDashboardEvent.bind(this));
     this.subsCastRangeUpdateTimeOut = this._dateTimeRageService.castRangeUpdateTimeout.subscribe(this.getData.bind(this));
     this.getData();
-  }
+    }
+
   ngAfterContentInit() {
 
-    // here is where the chart is build first time
-    this.buildD3Chart()
-    
-    }
+  this.buildD3Chart()
+  }
+
   private async onDashboardEvent(data: any) {
     const dataId = data.resultWidget[this.id];
     if (dataId && dataId.query) {
       if (this.lastTimestamp * 1 === dataId.timestamp * 1) {
         return;
       }
-
       this.lastTimestamp = dataId.timestamp * 1;
-
       this.localData = dataId.query;
     }
    this.getData();
@@ -193,189 +175,168 @@ ngOnInit() {
     return false;
   }
 
-getChartData = (arr) => {
-  let arrCount = [];
-  arr.forEach( key=> {
-    arrCount.push(key.count * 1 || 0)
-  })
-  return arrCount ;
-}
+  getChartData = (arr) => {
+    let arrCount = [];
+    arr.forEach( key=> {
+      arrCount.push(key.count * 1 || 0)
+    })
+    return arrCount ;
+  }
 
-getChartColors = (arr) => {
-  let arrColors = [];
-  arr.forEach( c => {
-    arrColors.push(c.color)
-  })
-  return arrColors;
-}
+  getChartColors = (arr) => {
+    let arrColors = [];
+    arr.forEach( c => {
+      arrColors.push(c.color)
+    })
+    return arrColors;
+  }
 
-selectedKeys = (data, selected) =>{
-  let keyArr = [];
-  if(data){
-    data.forEach(value => {
-      if(value[selected].toString()===''){
-        value[selected] = 'not set'
-      }else{
-        value[selected] = value[selected].toString()
+  selectedKeys = (data, selected) =>{
+    let keyArr = [];
+    if(data){
+      data.forEach(value => {
+        if(value[selected].toString()===''){
+          value[selected] = 'not set'
+        }else{
+          value[selected] = value[selected].toString()
+        }
+        keyArr.push(value[selected])
+      })
+    }
+    return keyArr;
+  }
+
+  getRandomColors(n){
+    let colorsArray = [];
+    for(let i = 0 ; i < n.length; i ++){
+      let letters = '0123456789abcdef';
+      let color = '#';
+      for(let j = 0 ; j < 6; j++){
+        color +=letters[Math.floor(Math.random()*16)];
       }
-      keyArr.push(value[selected])
+    colorsArray.push(color);
+    }
+    return colorsArray;
+  }
+
+  tagger = (kS, kA) => {
+    let tagArr = [];
+    for(let i = 0 ; i < kS.length; i++){
+      let keysCount = kA.filter(el => el.indexOf(kS[i])> -1).length;
+      let tag = new Tag();
+      tag.count = keysCount;
+      tag.color = this.colorScale[i];
+      if(this.keysSet[i]===''){
+        tag.name ='not set'
+      }else{
+        tag.name = this.keysSet[i];
+      }
+      tagArr.push(tag);
+      }
+      return tagArr;
+  }
+
+  getTagCounts = (selectedKeyArr, tagsObj, keysSet) => {
+    for(let i = 0 ; i < keysSet.length; i ++) {
+      let tagsCount = selectedKeyArr.filter(el => el.indexOf(keysSet[i]) > -1).length;
+      tagsObj[i].count = tagsCount *1 || 0;
+      }
+    }
+
+  private async getData() {
+
+    if (!this.localData) {
+      return;
+    } 
+
+    this.protocol_profile = this.localData.protocol_id;
+      if (this.localData.location && this.localData.location.value !== '' && this.localData.location.mapping !== '') {
+        this.configQuery.param.location[this.localData.location.mapping] = this.localData.location.value;
+      }
+
+    this.configQuery.param.search[this.protocol_profile] = this.localData.fields;
+    this.configQuery.timestamp = this._dateTimeRageService.getDatesForQuery(true);
+    const dataMapping: any = await this._prefMapProtoService.getAll().toPromise();
+    const result = await this._srcCallService.getData(this.configQuery).toPromise();
+    const dataMappingItem = dataMapping.data.filter(i => i.profile === this.protocol_profile.split('_')[1])[0];
+
+    if (dataMappingItem && dataMappingItem.fields_mapping) {
+      const fields_mapping = dataMappingItem.fields_mapping;
+      this.columnKeysGroupColumn = result.keys;
+      this.columnKeys = fields_mapping.filter(i => i.type !== 'string').map(i => i.id.split('.')[1]);
+      this.dataForChart = result.data;
+      this.selectedKeyArr = this.selectedKeys(this.dataForChart,this.keySelected);
+      this.keysSet = Array.from(new Set(this.selectedKeyArr));
+      this.colorScale =  this.getRandomColors(this.keysSet);
+      this.Tags.tagObj = this.tagger(this.keysSet, this.selectedKeyArr);
+      this.getTagCounts(this.selectedKeyArr, this.Tags.tagObj,this.keysSet);
+      this.saveConfig();
+      this.buildD3Chart();
+    }
+  }
+
+  buildD3Chart() {
+    this.chartKeys = []; 
+    let tagobject = this.Tags.tagObj;
+
+    tagobject.forEach((tag) => {
+      const target = new Tag();
+      target.name = tag.name;
+      target.color = tag.color;
+      target.count = tag.count;
+      this.chartKeys.push(target);
+    });
+
+    this.chartData = [];
+
+    this.chartKeys.forEach((key) => {
+      this.chartData.push(key.count);
+      this.chart.data = [...this.chartData];
     })
   }
-  return keyArr;
-}
 
-getRandomColors(n){
-  let colorsArray = [];
-  for(let i = 0 ; i < n.length; i ++){
-  let letters = '0123456789abcdef';
-  let color = '#';
-  for(let j = 0 ; j < 6; j++){
-    color +=letters[Math.floor(Math.random()*16)];
-  }
-  colorsArray.push(color);
-}
-return colorsArray;
-}
-
-tagger = (kS, kA) => {
-let tagArr = [];
-for(let i = 0 ; i < kS.length; i++){
-  let keysCount = kA.filter(el => el.indexOf(kS[i])> -1).length;
-  let tag = new Tag();
-  tag.count = keysCount;
-  tag.color = this.colorScale[i];
-  if(this.keysSet[i]===''){
-    tag.name ='not set'
-  }else{
-    tag.name = this.keysSet[i];
-  }
-  tagArr.push(tag);
-  }
-  return tagArr;
-}
-
-getTagCounts = (selectedKeyArr, tagsObj, keysSet) => {
-  for(let i = 0 ; i < keysSet.length; i ++) {
-    let tagsCount = selectedKeyArr.filter(el => el.indexOf(keysSet[i]) > -1).length;
-    tagsObj[i].count = tagsCount *1 || 0;
-    }
-  }
-
-private async getData() {
-   if (!this.localData) {
-    return;
-  } 
-
-  this.protocol_profile = this.localData.protocol_id;
-  if (this.localData.location && this.localData.location.value !== '' && this.localData.location.mapping !== '') {
-    this.configQuery.param.location[this.localData.location.mapping] = this.localData.location.value;
-  }
-
-  this.configQuery.param.search[this.protocol_profile] = this.localData.fields;
-
-  this.configQuery.timestamp = this._dateTimeRageService.getDatesForQuery(true);
-
-  const dataMapping: any = await this._prefMapProtoService.getAll().toPromise();
-
-  const result = await this._srcCallService.getData(this.configQuery).toPromise();
-
-  const dataMappingItem = dataMapping.data.filter(i => i.profile === this.protocol_profile.split('_')[1])[0];
-
-  if (dataMappingItem && dataMappingItem.fields_mapping) {
-
-    const fields_mapping = dataMappingItem.fields_mapping;
-
-    this.columnKeysGroupColumn = result.keys;
-
-    this.columnKeys = fields_mapping.filter(i => i.type !== 'string').map(i => i.id.split('.')[1]);
-
-    this.dataForChart = result.data;
- 
-    this.selectedKeyArr = this.selectedKeys(this.dataForChart,this.keySelected)
-
-    this.keysSet = Array.from(new Set(this.selectedKeyArr))
-
-    this.colorScale =  this.getRandomColors(this.keysSet)
-    
-    this.Tags.tagObj = this.tagger(this.keysSet, this.selectedKeyArr);
-
-    this.getTagCounts(this.selectedKeyArr, this.Tags.tagObj,this.keysSet);
-
-    this.saveConfig();
-
-    this.buildD3Chart();
-  }
-}
-
-buildD3Chart() {
-
-this.chartKeys = []; // use this reponse to test
-
-let tagobject = this.Tags.tagObj;
-
-tagobject.forEach((info) => {
-  const target = new Tag();
-  target.name = info.name;
-  target.color = info.color;
-  target.count = info.count;
-  this.chartKeys.push(target);
-});
-
-this.chartData = [];
-
-this.chartKeys.forEach((info) => {
-  this.chartData.push(info.count);
-  this.chart.data = [...this.chartData];
-})
-
-  
-}
-
-
-toggleData(event: MatSlideToggleChange) {
-  this.chartControlsService.showData = event.checked;
-}
-
-
-private saveConfig() {
-  const _f = Functions.cloneObject;
-  this.config = {
-    title: this.title || this.id,
-    configQuery: _f(this.configQuery),
-    groupColumnAxis1: this.groupColumnAxis1,
-    localData: this.localData,
-    isShowPanelSettings: this.isShowPanelSettings
-  };
-
-  this.changeSettings.emit({
-    config: _f(this.config),
-    id: this.id
-  });
-}
-
-async openDialog() {
-  const dialogRef = this.dialog.open(SettingDisplayChartD3WidgetComponent, {
-    data: {
+  private saveConfig() {
+    const _f = Functions.cloneObject;
+    this.config = {
       title: this.title || this.id,
+      configQuery: _f(this.configQuery),
+      groupColumnAxis1: this.groupColumnAxis1,
+      localData: this.localData,
+      isShowPanelSettings: this.isShowPanelSettings
+    };
+  
+    this.changeSettings.emit({
+      config: _f(this.config),
+      id: this.id
+    });
+  }
+
+
+  toggleData(event: MatSlideToggleChange) {
+    this.chartControlsService.showData = event.checked;
+  }
+
+  async openDialog() {
+    const dialogRef = this.dialog.open(SettingDisplayChartD3WidgetComponent, {
+      data: {
+        title: this.title || this.id,
+      }
+    });
+    const result = await dialogRef.afterClosed().toPromise();
+    if (!result) {
+      return;
     }
-  });
-
-  const result = await dialogRef.afterClosed().toPromise();
-  if (!result) {
-    return;
+    this.title = result.title;
+    this.saveConfig();
+  }
+  
+  changeKey(e:any){
+    this.keySelected = e.value;
+    this.getData()
   }
 
-  this.title = result.title;
-
-  this.saveConfig();
-}
-
-changeKey(e:any){
-  this.keySelected = e.value;
-  this.getData()
-}
-ngOnDestroy() {
-  this.subsDashboardEvent.unsubscribe()
-  this.subsCastRangeUpdateTimeOut.unsubscribe()
+  ngOnDestroy() {
+    this.subsDashboardEvent.unsubscribe()
+    this.subsCastRangeUpdateTimeOut.unsubscribe()
+    }
   }
-}
