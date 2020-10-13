d0dc14273c35eea66c9add951500c81a59d93adc
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2020-10-13 12:35:30 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,7 @@
 import { VERSION } from '../VERSION';
 
-declare var $ENV: Env;
-
-interface Env {
-  ENVIRONMENT: string;
-  apiUrl: string;
-}
-
 export const environment = {
   production: true,
   environment: VERSION + '(prod)',
-  apiUrl: $ENV.apiUrl || window.location.protocol + '//' + (window.location.host) + '/api/v3',
+  apiUrl: window.location.protocol + '//' + (window.location.host) + '/api/v3',
 };
