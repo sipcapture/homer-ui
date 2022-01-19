e532b2858bb59adcf371f97ed74fc5e7e3e2043a
Eduard Klimenko <edik.klimenko@gmail.com>
2022-01-19 16:16:34 +0200

    fix: https://github.com/sipcapture/homer/issues/518 sip flow ui - inability to scroll down after scrolling right

--- src/app/components/search-grid-call/detail-dialog/tab-flow/tab-flow.module.ts
+++ src/app/components/search-grid-call/detail-dialog/tab-flow/tab-flow.module.ts
@@ -2,17 +2,21 @@ import { FlowItemComponent } from './flow-item/flow-item.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabFlowComponent } from './tab-flow.component';
-import { VirtualScrollerModule } from 'ngx-virtual-scroller';
+// import { VirtualScrollerModule } from 'ngx-virtual-scroller';
 import { TranslateModule } from '@ngx-translate/core'
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
   imports: [
     CommonModule,
-    VirtualScrollerModule,
+    // VirtualScrollerModule,
+    ScrollingModule,
     TranslateModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTooltipModule
   ],
   declarations: [
     TabFlowComponent,
