10b99394ba467dddf75bcc51164a5ffc1a392a42
Alexey Oplachko <xirenec1@gmail.com>
2023-02-20 19:46:57 +0200

    Allowed multiple alerts to be shown at the same time

--- src/app/components/controls/alert/alert-overlay.service.ts
+++ src/app/components/controls/alert/alert-overlay.service.ts
@@ -1,21 +1,15 @@
 import { Overlay, OverlayConfig, OverlayRef } from "@angular/cdk/overlay";
-import { ComponentPortal, PortalInjector } from "@angular/cdk/portal";
-import { ComponentRef, Injectable, Injector } from "@angular/core";
+import { ComponentPortal } from "@angular/cdk/portal";
+import { ComponentRef, Injectable, Injector, StaticProvider } from "@angular/core";
 import { AlertComponent } from "@app/components";
+import { AlertSubject } from "@app/models/alert.model";
 import { AlertOverlayRef } from "./alert-ref";
 import { ALERT_OVERLAY } from "./alert.tokens";
-
-export interface Message {
-    text: string;
-    object?: string;
-    type: string;
-}
-
 interface AlertDialogConfig {
     panelClass?: string;
     hasBackdrop?: boolean;
     backdropClass?: string;
-    message?: Message;
+    message?: AlertSubject;
 }
 const DEFAULT_CONFIG: AlertDialogConfig = {
     hasBackdrop: false,
@@ -26,50 +20,43 @@ const DEFAULT_CONFIG: AlertDialogConfig = {
 @Injectable()
 export class AlertOverlayService {
     isOpen = false;
-    dialogRef;
+    dialogRef: AlertOverlayRef | null;
     timeoutId;
-    overlayComponent;
+    overlayComponent: AlertComponent;
     constructor(private injector: Injector, private overlay: Overlay) {}
 
     open(config: AlertDialogConfig = {}) {
         if (this.dialogRef) {
-            this.resetTimer();
-            this.dialogRef.close();
-        }
-        // Override default configuration
-        const dialogConfig = { ...DEFAULT_CONFIG, ...config };
+            this.overlayComponent.addNotification(config.message)
+            return this.dialogRef;
+        } else {
+            // Override default configuration
+            const dialogConfig = { ...DEFAULT_CONFIG, ...config };
 
-        // Returns an OverlayRef which is a PortalHost
-        const overlayRef = this.createOverlay(dialogConfig);
+            // Returns an OverlayRef which is a PortalHost
+            const overlayRef = this.createOverlay(dialogConfig);
 
-        // Instantiate remote control
-        const dialogRef = new AlertOverlayRef(overlayRef);
-        this.dialogRef = dialogRef;
-        const overlayComponent = this.attachDialogContainer(
-            overlayRef,
-            dialogConfig,
-            dialogRef
-        );
-        this.overlayComponent = overlayComponent
-        overlayComponent.closeAlert.subscribe((_) => dialogRef.close());
-        overlayComponent.resetTimer.subscribe((_) => this.resetTimer());
-        overlayComponent.clearMessageTimer.subscribe((_) => this.clearMessageTimer())
-        overlayRef.backdropClick().subscribe((_) => dialogRef.close());
-        this.clearMessageTimer();
-        return dialogRef;
-    }
-
-    clearMessageTimer() {
-        if (!this.overlayComponent.isOpen) {
-            this.resetTimer();
-            this.timeoutId = setTimeout(() => {
-                this.dialogRef.close()
-            }, 5000);
+            // Instantiate remote control
+            const dialogRef = new AlertOverlayRef(overlayRef);
+            
+            this.dialogRef = dialogRef;
+        
+            const overlayComponent = this.attachDialogContainer(
+                overlayRef,
+                dialogConfig,
+                dialogRef
+            );
+            this.overlayComponent = overlayComponent
+            overlayComponent.closeAlert.subscribe((_) =>{ 
+                this.dialogRef.close(); 
+                this.dialogRef = null;
+            });
+            overlayRef.backdropClick().subscribe((_) => dialogRef.close());
+            return dialogRef;
         }
     }
-    resetTimer() {
-        clearTimeout(this.timeoutId);
-    }
+
+
     private createOverlay(config: AlertDialogConfig) {
         const overlayConfig = this.getOverlayConfig(config);
         return this.overlay.create(overlayConfig);
@@ -96,13 +83,12 @@ export class AlertOverlayService {
     private createInjector(
         config: AlertDialogConfig,
         dialogRef: AlertOverlayRef
-    ): PortalInjector {
-        const injectionTokens = new WeakMap();
+    ): Injector {
+        const injectionTokens2: StaticProvider[] = [];
 
-        injectionTokens.set(AlertOverlayRef, dialogRef);
-        injectionTokens.set(ALERT_OVERLAY, config.message);
-
-        return new PortalInjector(this.injector, injectionTokens);
+        injectionTokens2.push({provide: AlertOverlayRef, useValue: dialogRef});
+        injectionTokens2.push({provide: ALERT_OVERLAY, useValue: config.message});
+        return Injector.create({parent: this.injector, providers: injectionTokens2});
     }
 
     private getOverlayConfig(config: AlertDialogConfig): OverlayConfig {
--- src/app/components/controls/alert/alert.component.ts
+++ src/app/components/controls/alert/alert.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef, Injectable, Inject, EventEmitter, Output } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef, Inject, EventEmitter, Output } from '@angular/core';
 
-import { AlertService, CopyService } from './../../../services';
+import { CopyService, } from './../../../services';
 import { TranslateService } from '@ngx-translate/core'
 import { ALERT_OVERLAY } from './alert.tokens';
+import { Functions } from '@app/helpers/functions';
+import { AlertProper, AlertSubject, MessageTimer } from '@app/models/alert.model';
+
+
 @Component({
     selector: 'alert-overlay',
     templateUrl: 'alert.component.html',
@@ -12,36 +15,82 @@ import { ALERT_OVERLAY } from './alert.tokens';
 })
 
 export class AlertComponent implements OnInit, OnDestroy {
-    private subscription: Subscription;
     timeoutId: any;
     isOpen = false;
+    messages: Map<string, AlertProper> = new Map();
+    guidArray: Array<string> = [];
     constructor(
         public translateService: TranslateService,
         private copyService: CopyService,
-        @Inject(ALERT_OVERLAY) public message: any
+        private cdr: ChangeDetectorRef,
+        @Inject(ALERT_OVERLAY) public message: AlertSubject
     ) {
+        this.addNotification(message, true)
         translateService.addLangs(['en'])
         translateService.setDefaultLang('en')
     }
     @Output() closeAlert = new EventEmitter();
-    @Output() clearMessageTimer = new EventEmitter();
-    @Output() resetTimer = new EventEmitter();
     ngOnInit() {
     }
-    clearMessage() {
-        this.closeAlert.emit();
+    clearMessage(guid: string) {
+        clearTimeout(this.messages.get(guid)?.timeout);
+        this.guidArray = this.guidArray.filter(guidFromArray => guidFromArray !== guid);
+        this.messages.delete(guid);
+        if (this.messages.size === 0) {
+            this.closeAlert.emit();
+        }
+        this.cdr.detectChanges();
+    }
+    resetTimer(guid: string) {
+        clearTimeout(this.messages.get(guid)?.timeout);
+    }
+    clearMessageTimer(guid: string) {
+        const message = this.messages.get(guid);
+        if (!message.isOpen) {
+            this.resetTimer(guid);
+            const message = this.messages.get(guid);
+            message.timeout = setTimeout(() => {
+                this.clearMessage(guid);
+            }, MessageTimer);
+            this.messages.set(guid, message);
+        }
     }
     ngOnDestroy() {
     }
-    toggleFull() {
-        this.isOpen = !this.isOpen;
-        if (this.isOpen) {
-            this.resetTimer.emit();
+    toggleFull(guid: string) {
+        const message = this.messages.get(guid);
+        message.isOpen = !message?.isOpen;
+        this.messages.set(guid, message)
+        if (message.isOpen) {
+            this.resetTimer(guid);
         } 
     }
-    copy() {
-        this.translateService.get('notifications.success.notificationCopy').subscribe(alert => {
-            this.copyService.copy(this.message.object, alert)
-        })
+    addNotification(message: AlertSubject, firstBoot: boolean = false) {
+        const guid = Functions.newGuid();
+        const timeout = setTimeout(() => {
+            this.clearMessage(guid);
+        }, MessageTimer);
+        const messageProper: AlertProper = {...message, timeout, isOpen: false }
+        
+        if (this.guidArray.length >= 3) {
+            const removedGuid = this.guidArray.shift();
+            this.messages.delete(removedGuid);
+        }
+        this.guidArray.push(guid);
+        this.messages.set(guid, messageProper);
+
+        if (!firstBoot) {
+            this.cdr.detectChanges();
+        }
+    }
+    copy(guid) {
+        const message = this.messages.get(guid);
+        this.copyService.copy(message.object, {
+            message: 'notifications.success.notificationCopy',
+            isTranslation: true,
+        });
+    }
+    returnZero() {
+        return 0;
     }
 }
