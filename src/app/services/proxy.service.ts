1eade8cf8c763f466b022bcabcbc590a0fc499b2
Alexey Oplachko <xirenec1@gmail.com>
2023-06-12 21:54:25 +0300

    #593 added grafana path from api

--- src/app/services/proxy.service.ts
+++ src/app/services/proxy.service.ts
@@ -7,35 +7,38 @@ import { environment } from '@environments/environment';
 @Injectable({ providedIn: 'root' })
 
 export class ProxyService {
-  private url = `${environment.apiUrl}/proxy`;
+    private url = `${environment.apiUrl}/proxy`;
 
-  constructor(private _http: HttpClient) {}
+    constructor(private _http: HttpClient) { }
 
-  // Get Folders list
-  getProxyGrafanaFolders(): Observable<any> {
-    return this._http.get<any>(`${this.url}/grafana/folders`);
-  }
-  getProxyGrafanaSearch(uid: string): Observable<any> {
-    return this._http.get<any>(`${this.url}/grafana/search/` + uid);
-  }
-  // Get Dashboard list
-  getProxyGrafanaDashboards(folder: string): Observable<any> {
-    return this._http.get<any>(`${this.url}/grafana/dashboards/uid/` + folder);
-  }
+    // Get Folders list
+    getProxyGrafanaFolders(): Observable<any> {
+        return this._http.get<any>(`${this.url}/grafana/folders`);
+    }
+    getProxyGrafanaSearch(uid: string): Observable<any> {
+        return this._http.get<any>(`${this.url}/grafana/search/` + uid);
+    }
+    // Get Dashboard list
+    getProxyGrafanaDashboards(folder: string): Observable<any> {
+        return this._http.get<any>(`${this.url}/grafana/dashboards/uid/` + folder);
+    }
 
-  // Get Grafana URL
-  getProxyGrafanaUrl(): Observable<any> {
-    return this._http.get<any>(`${this.url}/grafana/url`);
-  }
-  // Get Grafana OrgID
-  getProxyGrafanaOrg(): Observable<any> {
-    return this._http.get<any>(`${this.url}/grafana/org`);
-  }
-  getProxyGrafanaStatus(): Observable<any> {
-    return this._http.get<any>(`${this.url}/grafana/status`).pipe(catchError(this.handleError));
-  }
-  handleError(error: HttpErrorResponse){
-       
-    return throwError(error);
-}
+    // Get Grafana URL
+    getProxyGrafanaUrl(): Observable<any> {
+        return this._http.get<any>(`${this.url}/grafana/url`);
+    }
+    getProxyGrafanaPath(): Observable<any> {
+        return this._http.get<any>(`${this.url}/grafana/path`);
+    }
+    // Get Grafana OrgID
+    getProxyGrafanaOrg(): Observable<any> {
+        return this._http.get<any>(`${this.url}/grafana/org`);
+    }
+    getProxyGrafanaStatus(): Observable<any> {
+        return this._http.get<any>(`${this.url}/grafana/status`).pipe(catchError(this.handleError));
+    }
+    handleError(error: HttpErrorResponse) {
+
+        return throwError(error);
+    }
 }
