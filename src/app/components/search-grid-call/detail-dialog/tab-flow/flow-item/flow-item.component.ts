e6720c32823e863a2a5126e570f999390176df89
Daniel Donoghue <daniel.donoghue@aurorainnovation.com>
2025-02-27 12:59:10 +0100

    add new Capture-ID message consolidation feature

--- src/app/components/search-grid-call/detail-dialog/tab-flow/flow-item/flow-item.component.ts
+++ src/app/components/search-grid-call/detail-dialog/tab-flow/flow-item/flow-item.component.ts
@@ -9,6 +9,8 @@ import {
   ChangeDetectorRef,
 } from '@angular/core';
 import { Functions } from '@app/helpers/functions';
+import { Subscription } from 'rxjs';
+import { CallCaptureIdConsolidationService } from '@app/services/call/consolidation.service';
 
 @Component({
   selector: 'app-flow-item',
@@ -18,8 +20,14 @@ import { Functions } from '@app/helpers/functions';
 })
 export class FlowItemComponent implements AfterViewChecked {
   _item: any = {};
+  showSubItems: boolean = false;
+
+  consolidationFilterSubscription: Subscription;
+  expandedItemSubscription: Subscription;
+
   @Input() set item(val: any) {
     this._item = val;
+    this.mergeOptions();
   }
   get item(): any {
     return this._item;
@@ -28,16 +36,50 @@ export class FlowItemComponent implements AfterViewChecked {
   @Input() isGroupByAlias = false;
   @Input() idx = 0;
   @Input() isAbsolute: boolean = false;
-  @Output() itemClick: EventEmitter<any> = new EventEmitter();
+  @Output() itemClick: EventEmitter<{ idx: number, event: any, item: any }> = new EventEmitter();
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor(
+    private cdr: ChangeDetectorRef, 
+    private consolidationService: CallCaptureIdConsolidationService) {}
 
-  onClickItem(idx, event) {
-    this.itemClick.emit({ idx, event });
+  ngOnInit() {
+    this.expandedItemSubscription = this.consolidationService.expandedItem$.subscribe(expandedIdx => {
+      if (expandedIdx !== this.idx && this.showSubItems) {
+        this.showSubItems = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  ngOnDestory() {
+    if (this.consolidationFilterSubscription) this.consolidationFilterSubscription.unsubscribe();
+    if (this.expandedItemSubscription) this.expandedItemSubscription.unsubscribe();
+  }
+
+  onClickItem(item, idx, event) {
+    this.itemClick.emit({ idx, event, item });
+  }
+
+  toggleSubItems(event: Event) {
+    event.stopPropagation(); // Prevent triggering the onClickItem event
+    this.showSubItems = !this.showSubItems;
+    if (this.showSubItems) {
+      this.consolidationService.setExpandedItem(this.idx);
+    }    
   }
 
   MOSColorGradient(hue) {
     return Functions.MOSColorGradient(hue * 100, 80, 50);
+  }
+
+  private mergeOptions() {
+    if (this._item && this._item.subItems) {
+      this._item.subItems = this._item.subItems.map((subItem, iter) => ({
+        ...subItem,
+        options: this._item.options,
+        __item__subindex__: iter
+      }));
+    }
   }
 
   ngAfterViewChecked() {
